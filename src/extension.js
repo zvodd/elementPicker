ce5889961e740e7ba9763ad03462e311b05a3edc
zvodd <zv.odd.101@gmail.com>
2021-10-14 03:20:07 +0800

    ideas

---
+++ src/extension.js
@@ -0,0 +1,8 @@
+/**
+ * Thoughts:
+ *  Keeping access away from Page JS context?
+ *  Use shadow DOM Element + iframe
+ *  Source iframe from extension?
+ */
+
+
---
+++ src/main_wip.js
@@ -0,0 +1,18 @@
+/**
+ * Snipping usecases:
+ * 
+ * Single element 
+ * - striaght element picker
+ * 
+ * Parent Element
+ *  - Draw
+ * 
+ * Text Selection 
+ *  - export dom
+ *  - export text
+ * 
+ * Generate XPATH / CSS Selector
+ *  
+ * 
+ * HTML Clean, remove event handlers
+ */
