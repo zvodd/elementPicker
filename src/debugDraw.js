c759b3e009140c5a6ce1c3bc0ec6b76ebfde9cb7
zvodd <zv.odd.101@gmail.com>
2021-10-14 21:26:20 +0800

    shadow

--- src/debugDraw.js
+++ src/debugDraw.js
@@ -1,28 +1,29 @@
-debugDraw = ((docEl)=> {
+debugDraw = (()=> {
     let svgid = "qowujdo8whqd"
     function renderSVG(x,y,w,h){
-        let svgel = docEl.querySelector(`#${svgid}`)
-        if (svgel !== null){
-            docEl.removeChild(svgel)
-        }else{
-            console.log("wtf", svgel)
+        let wrap = document.querySelector(`#${svgid}`)
+        if (wrap !== null){
+            document.documentElement.removeChild(wrap)
         }
+        wrap = document.createElement("div")
+        wrap.id = svgid
+        document.documentElement.appendChild(wrap)
+        let shadow = wrap.attachShadow({mode: 'open'});
 
-        let bounds = document.body.getBoundingClientRect()//docEl.getBoundingClientRect()
+        let bounds = document.body.getBoundingClientRect()//document.getBoundingClientRect()
 
         let innerSVG = `
         <!-- <rect x="0" y="0" width="${bounds.width}" height="${bounds.height}" fill="transparent"/> -->
-        
         <rect x="${x + window.scrollX}" y="${y + window.scrollY}" width="${w}" height="${h}" stroke-width="5" stroke="orange" fill="#044B94" fill-opacity="0.4" />
         `
-        // <rect x="${x}" y="${y}" width="${w}" height="${h}" stroke-width="5" stroke="orange" fill="#044B94" fill-opacity="0.4" />
         let svgdoc = makeSvg(innerSVG, bounds.width, bounds.height)
-        docEl.appendChild(document.importNode(svgdoc, true))
+
+        shadow.appendChild(document.importNode(svgdoc, true))
     }
 
     function makeSvg(innerSVG, width, height){
         let svgdoc = new DOMParser().parseFromString(
-    `<svg xmlns="http://www.w3.org/2000/svg" id="${svgid}"
+    `<svg xmlns="http://www.w3.org/2000/svg" 
             style="position: absolute; top: 0; left: 0; z-index: 99999;"
             viewbox="0 0 ${width} ${height}"
             width="${width}"
